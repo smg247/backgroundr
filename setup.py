0a7e97e3447c1a67835d7fe8d848cfd0cfb02e57
Stephen Goeddel <sgoeddel247@gmail.com>
2015-02-14 02:07:16 -0500

    added a manifest

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="Backgroundr",
-    version="1.0.4",
+
+    keywords="Backgrounds Wallpapers Images",
+
+    version="1.1.0",
+
     install_requires=[
         "Pillow",
     ],
+
     packages=find_packages(),
+
     url="https://github.com/smg247/backgroundr",
+
     author="Stephen Goeddel",
+
     author_email="stephen@stephengoeddel.com"
+
 )
